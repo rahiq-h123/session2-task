9e205e0721743d54e7a65931d64af5199f25fab2
rahiq-h123 <rahiqh123@gmail.com>
2022-09-12 07:05:28 +0200

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1; $num2; $operator;
+    switch ($operator) {
+        case "+":
+           echo $num1 + $num2;
+            break;
+        case "-":
+            echo  $num1 - $num2;
+            break;
+        case "*":
+            echo  $num1 * $num2;
+            break;
+        case "/":
+            echo  $num1 / $num2;
+    }
+    
+?>
